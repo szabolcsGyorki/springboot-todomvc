a5d2ebabf7e7737126bd9bfede7b8cde9b48cd14
szabolcsGyorki <sz.gyorki@gmail.com>
2018-10-04 18:15:50 +0200

    Initial commit

---
+++ src/main/java/com/codecool/todomvc/TodomvcApplication.java
@@ -0,0 +1,12 @@
+package com.codecool.todomvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodomvcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TodomvcApplication.class, args);
+    }
+}
